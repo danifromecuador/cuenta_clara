fbc7806bc42a4a3dc61be3fe05a979c0c6030483
dani <danifromecuador@gmail.com>
2024-12-13 17:24:08 -0500

    Install ESlint & Stylelint linters feat/setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       Hello Dani
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
